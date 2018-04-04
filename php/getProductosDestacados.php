42225ff7b543ec5feacf18822029bda9e88e2009
Luis Sanchez <luissan9@live.com>
2018-04-03 23:01:09 -0500

    Se modifico la forma de obtener productos destacados si no hay productos destacados en tu localidad se muestras productos destacados en general

--- php/getProductosDestacados.php
+++ php/getProductosDestacados.php
@@ -2,15 +2,17 @@
     header("Content-Type:application/json");
     include_once("conectar.php");
     include("busqueda.php");
+    $sql = "SELECT `busqueda`,COUNT(*) AS n FROM `busquedas`";
     if(isset($_POST['estado'])){
         $estado = $_POST['estado'];
-    }else{
-        $estado="";
+        $sql .= " WHERE `estado` LIKE ?";
     }
     
-    $sql = "SELECT `busqueda`,COUNT(*) AS n FROM `busquedas` WHERE `estado` = ? GROUP BY `busqueda` ORDER BY n DESC LIMIT 6 ";
+    $sql .= " GROUP BY `busqueda` ORDER BY n DESC LIMIT 6 ";
     if ($query = $enlace->prepare($sql)){
-        $query -> bind_param("s",$estado);
+        if(isset($_POST['estado'])){
+            $query -> bind_param("s",$estado);
+        }
         $query -> execute();
         $res = $query->get_result();
         $query -> close();
@@ -34,21 +36,21 @@
         $val = $result[$i];
         $sql = "SELECT P.*, GROUP_CONCAT(T.tag) AS tags FROM productos P INNER JOIN map_tag M ON M.ID_producto = P.Id_producto INNER JOIN tags T ON T.ID_tag = M.ID_tag WHERE P.Id_producto = ?";
         if($query = $enlace->prepare($sql)){
-            $query->bind_param("i",$val);
-            $query->execute();
-            $res=$query->get_result();
-            $query->close();
+            $query -> bind_param("i",$val);
+            $query -> execute();
+            $res = $query -> get_result();
+            $query -> close();
         }else{
             echo json_encode(response(300,sqlError($sql,"i",$val)));
             return;
         }
         $sql = "SELECT path, Id_usuario FROM imagen_prod WHERE Id_producto = ? LIMIT 1";
         if($query = $enlace->prepare($sql)){
-            $query->bind_param("i",$val);
-            $query->execute();
-            $query->bind_result($img,$id_usuario);
-            $query->fetch();
-            $query->close();
+            $query -> bind_param("i",$val);
+            $query -> execute();
+            $query -> bind_result($img,$id_usuario);
+            $query -> fetch();
+            $query -> close();
         }else{
             echo json_encode(response(300,sqlError($sql,"i",$val)));
             return;
