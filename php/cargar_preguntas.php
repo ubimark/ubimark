23ff93577cbd512b0ce0d7ff98eb5650dab32ecd
Luis Sanchez <luissan9@live.com>
2018-04-11 23:06:01 -0500

    v1.1.7 Servicio para enviar preguntas Servicio para buscar preguntas con sus respuestas Estilos para visualizar las preguntas y sus respuestas
    Signed-off-by: Luis Sanchez <luissan9@live.com>

--- php/cargar_preguntas.php
+++ php/cargar_preguntas.php
@@ -2,7 +2,7 @@
     header("Content-Type:application/json");
     include_once("conectar.php");
     include_once("funciones.php");
-    $date = new \DateTime('now', new \DateTimeZone('America/Mexico_City'));
+   
     $Id_producto = $_POST['Id_producto'];
     $sql = "SELECT p.Id_pregunta,p.pregunta,p.fecha, CONCAT(SUBSTRING_INDEX(u.nombre,' ',1),' ',SUBSTRING(u.apellidos,1,1),'.') as cliente FROM preguntas as p JOIN usuario as u ON u.Id_usuario = p.Id_cliente WHERE p.Id_producto = ? ORDER BY p.Id_pregunta DESC" ;
     if($query = $enlace -> prepare($sql)){
@@ -10,39 +10,62 @@
         $query -> execute();
         $res = $query -> get_result();
         $query -> close();
+    }else{
+        echo json_encode(response(300,sqlError($sql,"i",$Id_producto)));
     }
     $preguntas = array();
     while($row = $res -> fetch_Assoc()){
-        $fecha = new DateTime($row['fecha']);
+        $row['fecha'] = get_diferencia($row['fecha']);
+        $sql2 = "SELECT * FROM respuestas WHERE Id_pregunta = ?";
+        if($query = $enlace -> prepare($sql2)){
+            $query -> bind_param("i",$row['Id_pregunta']);
+            $query -> execute();
+            $res2 = $query -> get_result();
+            $query -> close();
+        }else{
+            echo json_encode(response(300,sqlError($sql,"i",$row['Id_pregunta'])));
+        }
+        $row['respuestas'] = array();
+        while($row2 = $res2->fetch_Assoc()){
+            array_push($row['respuestas'],$row2);
+        }
+        array_push($preguntas,$row);
+    }
+    if($preguntas != []){
+        echo json_encode(response(200,$preguntas));
+    }else{
+        echo json_encode(response(305));
+    }
+    
+    return;
+    function get_diferencia($fecha){
+        $date = new \DateTime('now', new \DateTimeZone('America/Mexico_City'));
+        $fecha = new DateTime($fecha);
         $actual = time();
         $actual = $date->format("Y-m-d H:i:s");
         $actual = new DateTime($actual);
         $diff = $fecha -> diff($actual);
         if(strcmp($diff->format('%m'), "0")!=0){
-            $row['fecha'] = $fecha -> format("d-m-Y");
+            return $fecha -> format("d-m-Y");
         }else if(strcmp($diff->format('%d'), "1")==0){
-            $row['fecha'] = $diff->format('hace %d dia');
+            return $diff->format('hace %d dia');
         }else if(strcmp($diff->format('%d'), "0")!=0){
-            $row['fecha'] = $diff->format('hace %d días');
+            return $diff->format('hace %d días');
         }else if(strcmp($diff->format('%h'), "1")==0){
-            $row['fecha'] = $diff->format('hace %h hora');
+            return $diff->format('hace %h hora');
         }else if(strcmp($diff->format('%h'), "0")!=0){
-            $row['fecha'] = $diff->format('hace %h horas');
+            return $diff->format('hace %h horas');
         }else if(strcmp($diff->format('%i'), "1")==0){
-            $row['fecha'] = $diff->format('hace %i minuto');
+            return $diff->format('hace %i minuto');
         }else if(strcmp($diff->format('%i'), "0")!=0){
-            $row['fecha'] = $diff->format('hace %i minutos');
+            return $diff->format('hace %i minutos');
         }else if(strcmp($diff->format('%s'), "1")==0){
-            $row['fecha'] = $diff->format('hace %s segundo');
+            return $diff->format('hace %s segundo');
         }else if(strcmp($diff->format('%s'), "0")!=0){
-            $row['fecha'] = $diff->format('hace %s segundos');
+            return $diff->format('hace %s segundos');
         }else if(strcmp($diff->format('%s'), "0")==0){
-            $row['fecha'] = $diff->format('justo ahora');
+            return $diff->format('justo ahora');
         }
-        $token = bin2hex(random_bytes(128));
-        $expira = $date->format("Y-m-d H:i:s");
-        $row['respuestas'] = array( array("respuesta" => "Fierro"));
-        array_push($preguntas,$row);
     }
-    echo json_encode(response(200,$preguntas));
 ?>
+
