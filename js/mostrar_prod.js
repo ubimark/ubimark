efbae9adb8d2a722c3c742cbc437580a132ea6c1
luis <luissan9@live.com>
2018-06-16 00:58:09 -0500

    v1.2.10 Mostrar producto a html Arreglado bug no cargaban preguntas si no cargaba el mapa

--- js/mostrar_prod.js
+++ js/mostrar_prod.js
@@ -1,4 +1,5 @@
 var band_empresa = false;
+var coords;
 /**
  * Funcion para agregar un producto al carrito
  * 
@@ -10,7 +11,7 @@ function add2cart(id, cantidad) {
         href("paginas/login.html");
     }
     $.ajax({
-        url:api("carrito.php"),
+        url: api("carrito.php"),
         type: "post",
         dataType: "json",
         data: {
@@ -36,41 +37,120 @@ function add2cart(id, cantidad) {
 
 }
 
+function cargarImagenes(images){
+    let i = 1;
+    images.forEach(image => {
+        $("#carousel_body").append(
+            '<div class = "carousel-item carousel-item-prod ' + (i == 1 ? "active" : "") + '" data-slide-number = ' + i + ' >' +
+            '<img src = "../intranet/usuarios/' + image.uploadedBy + '/uploads/' + image.path + '" class = "img-fluid img-resize" >' +
+            '</div>');
+        $("#carousel_ind").append('<li class= "' + (i == 0 ? "active" : "") + '" data-slide-to="' + i + '" data-target="#myCarousel"></li>')
+        i++;
+    });
+    $("#carousel_body").append('<a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">' +
+        '<em class="fa fa-arrow-circle-o-left fa-2x" style="color: gray;" aria-hidden="true"></em>' +
+        '<span class="sr-only">Previous</span>' +
+        '</a>' +
+        '<a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">' +
+        '<em class="fa fa-arrow-circle-o-right fa-2x" style="color: gray;" aria-hidden="true"></em>' +
+        '<span class="sr-only">Next</span>' +
+        '</a>');
+}
+
+function cargarInfoVendedor(vendedor){
+    $("#empresa").html(vendedor.nombre);
+    let i=1;
+    vendedor.addrs.forEach(addr => {
+        $("#addr"+i).html(addr);
+        i++;
+    });
+    i=vendedor.calificacion;
+    $("#calificacion").html("("+i+")");
+    while(i>0){
+        if(i>=1){
+            star = "fa-star";
+        }else{
+            star = "fa-star-half";
+        }
+        $("#stars").append('<i class="fa '+star+' colorStar"></i>');
+        i--;
+    } 
+}
+
 function cargarPreguntas() {
     $("#seccion_preguntas").html("");
     $.ajax({
-        url:api("preguntas.php?Id_producto="+getParameterByName('key')),
+        url: api("preguntas.php?Id_producto=" + getParameterByName('key')),
         dataType: "json",
         type: "get"
     }).done(function (result) {
         switch (result.status_code) {
             case 200:
                 datos = result.data;
-                 
-                for(var pregunta of datos){
+
+                for (var pregunta of datos) {
                     $("#seccion_preguntas").append(
-                        '<div class="background-grey col-12 rounded p-2 mb-3 " id="pregunta_'+pregunta.Id_pregunta+'">' +
-                        '<span class="px-1">'+pregunta.cliente+'</span>'+
-                        '<span class="px-2 float-right fecha">'+pregunta.fecha+'</span>'+
-                        '<span class="d-block px-2 pregunta">'+pregunta.pregunta+'</span>' +
+                        '<div class="background-grey col-12 rounded p-2 mb-3 " id="pregunta_' + pregunta.Id_pregunta + '">' +
+                        '<span class="px-1">' + pregunta.cliente + '</span>' +
+                        '<span class="px-2 float-right fecha">' + pregunta.fecha + '</span>' +
+                        '<span class="d-block px-2 pregunta">' + pregunta.pregunta + '</span>' +
                         '</div>'
                     );
-                    for(var respuesta of pregunta.respuestas){
-                        $('#pregunta_'+pregunta.Id_pregunta).append(
-                            '<span class="ml-2 pl-3 respuesta">'+respuesta.respuesta+'</span>' 
+                    for (var respuesta of pregunta.respuestas) {
+                        $('#pregunta_' + pregunta.Id_pregunta).append(
+                            '<span class="ml-2 pl-3 respuesta">' + respuesta.respuesta + '</span>'
                         );
                     }
                 }
-                
+
                 break;
             case 305:
                 $("#seccion_preguntas").append(
                     '<div class="background-grey col-12 rounded p-2 mb-3 ">' +
-                    '<span class="d-block px-2 pregunta text-center">Aún no hay preguntas para este producto, se el primero en realizar una!</span>'+
+                    '<span class="d-block px-2 pregunta text-center">Aún no hay preguntas para este producto, se el primero en realizar una!</span>' +
                     '</div>'
                 );
                 break;
         }
+    }).always(function(){
+        initMap(band_empresa);
+    });
+}
+
+function cargarProducto() {
+    key = getParameterByName('key');
+    $.ajax({
+        url: api("producto.php?key=" + key),
+        type: "get",
+        dataType: "JSON"
+    }).done(function (res) {
+        switch (res.status_code) {
+            case 200:
+                data = res.data
+                cargarImagenes(data.images);
+                switch(data.tipo_cuenta){
+                    case "EMPRESA":
+                        coords = data.vendedor.coordenadas.split(",");
+                        set_band_empresa(true);
+                        $("#addrEmp").removeClass("d-none");
+                        cargarInfoVendedor(data.vendedor)
+                        break;
+                    case "PERSONAL":
+                        getCoords(data.vendedor.addrs[0]);
+                        set_band_empresa(false);
+                        cargarInfoVendedor(data.vendedor);
+                        break;
+                }
+                
+                $("#nom-producto").html(data.nombre_producto);
+                $("#precio").html("$ "+ parseFloat(data.precio).toFixed(2));
+                $("button.add2cart").prop("id",data.Id_producto);
+                $("#btn-comprar").attr("folio",data.Id_producto);
+                $("#description").html(data.descripcion);
+                $("#send_pregunta").attr("producto",data.Id_producto);
+        }
+    }).always(function(){
+        cargarPreguntas();
     });
 }
 /**
@@ -81,7 +161,7 @@ function cargarPreguntas() {
  */
 function comprarProducto(Id) {
     $.ajax({
-        url:api("comprarProducto.php"),
+        url: api("comprarProducto.php"),
         dataType: "json",
         type: "post",
         data: {
@@ -105,8 +185,8 @@ function comprarProducto(Id) {
 function initMap(empresa = true) {
     var zoom = empresa ? 15 : 13;
     var coords = {
-        lat: parseFloat($("#lat").val()),
-        lng: parseFloat($("#lon").val())
+        lat: parseFloat(window.coords[0]),
+        lng: parseFloat(window.coords[1])
     };
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: zoom,
@@ -144,14 +224,15 @@ function getCoords(location) {
         }
     }).done(function (data) {
         datos = data.results[0].geometry.location;
-        $("#lat").val(datos.lat);
-        $("#lon").val(datos.lng);
+        coords = new Array();
+        coords[0]=datos.lat;
+        coords[1]=datos.lng;
     });
 }
 
 function send_pregunta(producto) {
     $.ajax({
-        url:api("preguntas.php"),
+        url: api("preguntas.php"),
         dataType: "json",
         type: "post",
         data: {
@@ -166,7 +247,7 @@ function send_pregunta(producto) {
                 $("#pregunta").val('');
                 cargarPreguntas();
                 datos = result.data;
-                send_notificacion("PREGUNTA",datos.user,datos.target,datos.destino,datos.fecha,datos.tipo);
+                send_notificacion("PREGUNTA", datos.user, datos.target, datos.destino, datos.fecha, datos.tipo);
                 break;
         }
 
@@ -179,8 +260,9 @@ function set_band_empresa(val) {
 
 
 $(document).ready(function (e) {
-    initMap(band_empresa);
-    cargarPreguntas();
+    cargarProducto();
+    
+    //cargarPreguntas();
     $(".add2cart").click(function (e) {
         e.preventDefault();
         add2cart(this.id, $("#cantidad").val());
@@ -202,4 +284,5 @@ $(document).ready(function (e) {
             $("#pregunta").addClass("border-danger");
         }
     });
+    
 });
